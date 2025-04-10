3dab8d927e3a60b14e8198b85df469f1f73ccb05
arya4360 <abhishekarya436@gmail.com>
2025-04-11 00:18:14 +0530

    chore: update dependencies and add Tailwind CSS configuration
    - Added @supabase/supabase-js and updated Tailwind CSS dependencies in package.json
    - Updated postcss.config.mjs to include content paths for Tailwind CSS

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,5 +1,7 @@
 const config = {
   plugins: ["@tailwindcss/postcss"],
+  // tailwind.config.js
+  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
 };
 
 export default config;
